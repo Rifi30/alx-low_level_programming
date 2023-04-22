43f1bb330578ca4a2356cfc2dbf1ebe7746fc26d
Refilwe <kgoperefilwee@gmail.com>
2023-04-22 08:15:03 -0700

    task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    puts("Hello, world!");
-    puts("Program executed successfully.");
-    return 0;
+{
+puts("Hello, world!");
+puts("Program executed successfully.");
+return 0;
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,2 +1,7 @@
 0-preprocessor
-1-compiler 
+1-compileri
+2-assembler  
+3-name  
+4-puts.c  
+5-printf.c  
+6-size.c 
